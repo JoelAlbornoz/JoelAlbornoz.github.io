2ab29a5e5faf9e2ba141900206a85d437671a08a
JoelAlbornoz <39037260+JoelAlbornoz@users.noreply.github.com>
2019-12-11 11:40:03 -0300

    First commit - New portoflio page (2019)

---
+++ precache-manifest.971c9942b8f2e06b85e4ee6b86ed4fb8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e00303fb49feda00e3e7fec65d71165",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d3a456efbe4907da4c40",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "82bde25c7057ee594b25",
+    "url": "./static/js/2.4d2d2bc3.chunk.js"
+  },
+  {
+    "revision": "5ba8ceb64eea848335a496f62ab9063a",
+    "url": "./static/js/2.4d2d2bc3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3a456efbe4907da4c40",
+    "url": "./static/js/main.015792a5.chunk.js"
+  },
+  {
+    "revision": "de2204637e13d460dfa9",
+    "url": "./static/js/runtime-main.308be045.js"
+  },
+  {
+    "revision": "63812eb9c98cc1a0964009e466a1ebf5",
+    "url": "./static/media/Bootstrap.63812eb9.svg"
+  },
+  {
+    "revision": "8df2bea7b78da080a07a0efd4340507d",
+    "url": "./static/media/acr.8df2bea7.gif"
+  },
+  {
+    "revision": "ee53b9fe1a57c5136731c871f80d4ec0",
+    "url": "./static/media/css3.ee53b9fe.svg"
+  },
+  {
+    "revision": "3ce05f40580f35e8aa5899b1ef1b27a5",
+    "url": "./static/media/database.3ce05f40.svg"
+  },
+  {
+    "revision": "def9e5acde36edd57a3bd46b6212ca0a",
+    "url": "./static/media/foto.def9e5ac.jpg"
+  },
+  {
+    "revision": "1e8558a84f3b16656a5932b5d25d56aa",
+    "url": "./static/media/html5.1e8558a8.svg"
+  },
+  {
+    "revision": "e69f14ae01588e1115c2cc704a454eaf",
+    "url": "./static/media/java.e69f14ae.svg"
+  },
+  {
+    "revision": "11d39e6b8146bd55402210c9da841d66",
+    "url": "./static/media/js-square-brands.11d39e6b.svg"
+  },
+  {
+    "revision": "ad2b9f8e1d6164e76fa28d09088b66a8",
+    "url": "./static/media/material.ad2b9f8e.svg"
+  },
+  {
+    "revision": "40954ab831bd48e1a510e6f4079ed60e",
+    "url": "./static/media/prj.40954ab8.gif"
+  },
+  {
+    "revision": "302204afb56a08b0750f99c8d20a4ba8",
+    "url": "./static/media/react.302204af.svg"
+  }
+]);
